c2be8e4c828eb478990618dd634168275216ff23
synthMoza <eganian.aa@phystech.edu>
2021-07-13 20:47:15 +0300

    Add tests, add test script

---
+++ 4task_paracl/launch_tests.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+# Declare variables
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+PURPLE='\033[0;35m'
+EXEC='out'
+DIR='tests'
+
+count=0;
+count_success=0;
+count_failed=0;
+echo -e "-------------ParaCL Test Script-------------" 
+for file in tests/*.test; do
+	filename=$(basename -- "$file")
+	echo -e ${BLUE}
+    echo -e "[TEST_$count] \t ${NC} \n[NAME] \t\t ${PURPLE} $filename ${NC}"
+
+    # Define input and output filenames
+    input_file_name="${DIR}/${filename%.*}_input.txt";
+    output_file_name="${DIR}/${filename%.*}_output.txt";
+    output="${DIR}/output.txt";
+    
+    # Launch the test with this input
+    >$output; # clear the output file
+	./$EXEC $file < "$input_file_name" > $output
+	# Check the output
+	if cmp -s "$output" "$output_file_name"; then
+		echo -e "[RESULT] \t ${GREEN} SUCCESS ${NC}"
+		((count_success=count_success+1))
+	else
+		echo -e "[RESULT] \t ${RED} FAIL ${NC}"
+		((count_failed=count_failed+1))
+	fi
+
+    ((count=count+1))
+done
+
+	echo -e "${PURPLE}"
+	echo -e "Successful tests: ${count_success}, failed tests: ${count_failed}"
+
+rm "$output"
